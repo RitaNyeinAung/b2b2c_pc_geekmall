371986dec3318ee1c457c6bd33feb494532b5fb0
Rita Nyein Aung <ritanyeinaung.ec@gmail.com>
2022-02-16 12:03:18 +0630

    first commit

---
+++ public/config.js
@@ -0,0 +1,19 @@
+
+// ***** 前端配置文件 *****
+
+// 注意：只能更换域名，切勿更改后缀，以免访问不了接口
+
+// @param request 
+// 后端请求地址 请更新到当前服务器后端接口域名 
+// 例如：http://后端接口服务器域名/pc/public/index.php/
+let request = 'http://api.shopsn.net/pc/public/index.php/'
+
+// @param imgRequest 
+// 后端文件请求地址 用于存储图片等文件 请更新到当前服务器后端接口域名 
+// 例如：http://后端文件服务器域名/
+let imgRequest = 'http://center.shopsn.net/'
+
+
+// @param uploadImage
+// 文件上传前缀 不用更改
+let uploadImage = 'upload.php/'
